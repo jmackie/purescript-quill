024e7fdccc26b911e8b3c603da948492b8d33812
Jordan Mackie <jmackie@protonmail.com>
2018-01-26 09:34:17 +0000

    Elaborate FormatSpec reading

--- src/Quill/API/FormatSpec.purs
+++ src/Quill/API/FormatSpec.purs
@@ -14,30 +14,31 @@ import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except.Trans (except)
 
 import Data.Either (note)
-import Data.Foreign (F, Foreign, ForeignError(..), readString)
+import Data.Foreign (F, Foreign, ForeignError(..),
+                     readString, readBoolean, readNumber, readInt)
 import Data.List.NonEmpty (singleton)
 import Data.Tuple (Tuple(..))
 
 -- | https://quilljs.com/docs/formats/
 data FormatSpec
     = BackgroundColor Color
-    | Bold Boolean
     | TextColor Color
+    | Bold Boolean
     | Font FontName
     | InlineCode Boolean
     | Italic Boolean
     | Link String
     | Size Number
     | Strikethrough Boolean
-    | Superscript Boolean
-    | Subscript Boolean
+    -- TODO | Superscript
+    -- TODO | Subscript
     | Underline Boolean
     | Blockquote Boolean
     | Header Int
     | Indent Int
     -- TODO | List
     | TextAlignment Alignment
-    | TextDirection Boolean
+    -- TODO | TextDirection
     | CodeBlock Boolean
     -- TODO | Formula
     | Image URL
@@ -67,8 +68,44 @@ readFormatSpec (Tuple key value) =
                          ForeignError $ "invalid hex color string: " <> str))
             pure $ BackgroundColor color
 
+        "color" -> do
+            str   <- readString value
+            color <- except $ fromHexString str # note (singleton
+                        (ErrorAtProperty key $
+                         ForeignError $ "invalid hex color string: " <> str))
+            pure $ TextColor color
+
+        "bold"       -> readBoolean value   <#> Bold
+        "font"       -> readString value    <#> Font
+        "code"       -> readBoolean value   <#> InlineCode
+        "italic"     -> readBoolean value   <#> Italic
+        "link"       -> readString value    <#> Link
+        "size"       -> readNumber value    <#> Size
+        "strike"     -> readBoolean value   <#> Strikethrough
+        "underline"  -> readBoolean value   <#> Underline
+        "blockquote" -> readBoolean value   <#> Blockquote
+        "header"     -> readInt value       <#> Header
+        "indent"     -> readInt value       <#> Indent
+        "align"      -> readAlignment value <#> TextAlignment
+        "code-block" -> readBoolean value   <#> CodeBlock
+        "image"      -> readString value    <#> Image
+        "video"      -> readString value    <#> Video
+
+        -- Failure case
         prop -> do
             throwError $ (singleton
                 (ErrorAtProperty key $
                  ForeignError $ "unknown property"))
+
+readAlignment :: Foreign -> F Alignment
+readAlignment value = do
+    str <- readString value
+    case str of
+         "left"    -> pure Left
+         "center"  -> pure Center
+         "right"   -> pure Right
+         "justify" -> pure Justify
+         _ ->
+            throwError $ (singleton
+                (ForeignError $ "unknown alignment: " <> str))
 
