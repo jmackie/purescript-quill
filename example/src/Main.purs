1322aebac9f41c7b4bb21b2d4b069db7859a7b2c
Jordan Mackie <jmackie@protonmail.com>
2018-01-31 13:42:04 +0000

    Add to example

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -4,7 +4,9 @@ import Prelude
 
 import Color as Color
 
+import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Except (runExcept)
 
 import Data.Array as A
@@ -14,6 +16,7 @@ import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Options ((:=))
 import Data.String as S
+import Data.Tuple (Tuple(..))
 
 import DOM (DOM)
 import DOM.HTML (window)
@@ -26,63 +29,100 @@ import Partial (crashWith)
 import Partial.Unsafe (unsafePartial)
 
 import Quill as Q
-import Quill.API as QAPI
 import Quill.API (runAPI)
+import Quill.API as QAPI
 import Quill.API.Delta (Delta(..))
 import Quill.API.Formats as QFmt
+import Quill.API.Source as QSource
 import Quill.Config as QCfg
 import Quill.Types (QUILL)
 
-main :: forall e. Eff (dom :: DOM, quill :: QUILL | e) Unit
+main :: forall e. Eff (console :: CONSOLE, dom :: DOM, quill :: QUILL | e) Unit
 main = do
     target <- window
             >>= document
             >>= (htmlDocumentToParentNode
                 >>> querySelector (QuerySelector "#editor"))
             <#> (_ >>= elementToHTMLElement)
-
-    void $ case target of
-        Just el -> runAPI do
+    result <- runAPI $ case target of
+        Nothing ->
+            unsafePartial $ crashWith "editor element not found!"
+        Just el -> do
+            -- Initialise the editor
             let cfg = QCfg.debug       := QCfg.DebugWarn
                    <> QCfg.theme       := QCfg.SnowTheme
                    <> QCfg.placeholder := "Write here!"
-                   <> QCfg.formats     := [ QCfg.allow QFmt.bold
-                                          , QCfg.allow QFmt.italic
-                                          , QCfg.allow QFmt.underline
-                                          , QCfg.allow QFmt.header
-                                          , QCfg.allow QFmt.align
-                                          , QCfg.allow QFmt.color
-                                          ]
+                   <> QCfg.formats     :=
+                        [ QCfg.allow QFmt.bold
+                        , QCfg.allow QFmt.italic
+                        , QCfg.allow QFmt.underline
+                        , QCfg.allow QFmt.header
+                        , QCfg.allow QFmt.align
+                        , QCfg.allow QFmt.color
+                        ]
             editor <- Q.editor cfg el
 
-            _ <- QAPI.setContents
-                    [ Insert (Right "purescript-quill example\n") $
-                        QFmt.header := 1 <>
-                        QFmt.align  := QFmt.Center <>
-                        QFmt.color  := Color.fromInt 0xff0000
-                    , Insert (Right "Hello World!") $
-                        QFmt.italic := true
-                    , Insert (Right "\n") mempty
-                    ]
-                    Nothing
-                    editor
+            -- Set some initial content
+            void $ QAPI.setContents
+                        [ Insert (Right "purescript-quill example\n") $
+                            QFmt.header := 1 <>
+                            QFmt.align  := QFmt.Center <>
+                            QFmt.color  := Color.fromInt 0xff0000
+                        , Insert (Right "Hello World!") $
+                            QFmt.italic := true
+                        , Insert (Right "\n") mempty
+                        ]
+                        Nothing
+                        editor
 
-            _ <- QAPI.updateContents
-                    [ Retain (S.length "purescript-quill example\n") $
+            -- Update initial content
+            void $ QAPI.updateContents
+                        [ Retain (S.length "purescript-quill example\n") $
+                            mempty
+                        , Retain (S.length "Hello World") $
+                            QFmt.italic := true
+                        , Insert (Right " ") mempty
+                        , Delete 1
+                        , Insert (Right "?!") mempty
+                        , Insert (Right "\n") mempty
+                        ]
+                        Nothing
+                        editor
+
+            -- Insert a raw string at the end of the editor
+            void $ QAPI.getLength editor >>= \len ->
+                   QAPI.insertText
+                        len
+                        "I was inserted"
                         mempty
-                    , Retain (S.length "Hello World") $
-                        QFmt.italic := true
-                    , Insert (Right " ") mempty
-                    , Delete 1
-                    , Insert (Right "?!") mempty
-                    , Insert (Right "\n") mempty
-                    ]
-                    Nothing
-                    editor
+                        QSource.API
+                        editor
+
+            -- Get the editors current text
+            text <- QAPI.getText
+                        0
+                        Nothing -- use default
+                        editor
+            liftEff $ log text
+
+            -- Select everything
+            void $ QAPI.getLength editor >>= \len ->
+                   QAPI.setSelection
+                        (Tuple 0 len)
+                        QSource.API
+                        editor
 
             pure unit
 
-        Nothing -> unsafePartial $ crashWith "editor element not found!"
+    case result of
+        Left errs ->
+            unsafePartial $ crashWith $ "API call failed: " <> renderMultipleErrors errs
+        Right _ ->
+            pure unit
+
+
+    pure unit
+
 
 elementToHTMLElement :: Element -> Maybe HTMLElement
 elementToHTMLElement =
